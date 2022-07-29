37d64cd6aeec715cfe75585905e3368a9e3d9d2d
idrusma <marcello.surdi@gmail.com>
2022-07-29 11:54:02 +0200

    1.1.5

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -32,9 +32,13 @@ module.exports = ( env, argv ) => {
       },
 
       devServer: {
-        contentBase: paths.static,
-        watchContentBase: true,
+        static: {
+          directory: paths.static,
+          watch: true,
+        },
+
         hot: true,
+
         port: 3001,
       },
 
